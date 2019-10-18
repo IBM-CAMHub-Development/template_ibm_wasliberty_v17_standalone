5f14286ff0f49f6b40cfe32b2d857d241350ca7d
CAMHub PublishBot <octravis@us.ibm.com>
2019-10-18 13:39:42 +0000

    Automated CAMHub::publish on 2019-10-18 13:39:42 UTC

--- vmware/terraform/ibm_wasliberty_v17_standalone.tf
+++ vmware/terraform/ibm_wasliberty_v17_standalone.tf
@@ -360,7 +360,7 @@ variable "LibertyNode01_root_disk_size" {
 }
 
 module "provision_proxy" {
-  source 						= "git::https://github.com/IBM-CAMHub-Open/terraform-modules.git?ref=1.0//vmware/proxy"
+  source 						= "git::https://github.com/IBM-CAMHub-Development/terraform-modules.git?ref=1.0//vmware/proxy"
   ip                  = "${var.LibertyNode01_ipv4_address}"
   id									= "${vsphere_virtual_machine.LibertyNode01.id}"
   ssh_user            = "${var.LibertyNode01-os_admin_user}"
